2e7f08c02041db793ce166601e1babdc24e8bb90
nakayuji <xiongshizhongsi@gmail.com>
2019-11-09 14:44:22 +0900

    ローカルで表示できる

--- src/main/java/com/example/quizapp/QuizAppApplication.java
+++ src/main/java/com/example/quizapp/QuizAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizAppApplication {
-qq
+
 	public static void main(String[] args) {
 		SpringApplication.run(QuizAppApplication.class, args);
 	}
